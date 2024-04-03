512b8007935455a0b4383c2773444843d179404c
wanghongbing <wanghongbing@didiglobal.com>
2024-04-03 12:02:50 +0800

    OM HA support read from followers testing

---
+++ ozone/om-follower-read-test/follower-read-test.sh
@@ -0,0 +1,33 @@
+# create test bucket if not exist
+# bucket=test-follower-read and bucket=test-follower-read-linear test two cases
+vol=s3v
+bucket=test-follower-read
+ozone sh bucket info $vol/$bucket > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    ozone sh bucket create $vol/$bucket
+fi
+
+prefix=followerReadTest
+size=4096
+thread=100
+
+# WRITE
+ozone freon ockrw -r 1000000 -t $thread --linear --contiguous --duration 10s --size $size -v $vol -b $bucket -p $prefix
+
+sleep 3
+# READ
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=false --duration 30s --size $size -v $vol -b $bucket -p $prefix
+
+sleep 3
+# 50% WRITE 50% READ
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=false --duration 30s --size $size -v $vol -b $bucket -p $prefix
+
+# ONLY meta
+sleep 3
+# READ meta
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=true --duration 30s --size $size -v $vol -b $bucket -p $prefix
+
+sleep 3
+# 50% WRITE 50% READ meta
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=true --duration 30s --size $size -v $vol -b $bucket -p $prefix
+
