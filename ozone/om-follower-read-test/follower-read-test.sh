00d44375021da3425d6f744485858d36465b882c
wanghongbing <wanghongbing@didiglobal.com>
2024-04-08 19:30:30 +0800

    OM HA support read from followers testing fix time

--- ozone/om-follower-read-test/follower-read-test.sh
+++ ozone/om-follower-read-test/follower-read-test.sh
@@ -12,22 +12,22 @@ size=4096
 thread=100
 
 # WRITE
-ozone freon ockrw -r 1000000 -t $thread --linear --contiguous --duration 10s --size $size -v $vol -b $bucket -p $prefix
+ozone freon ockrw -r 1000000 -t $thread --linear --contiguous --duration 120s --size $size -v $vol -b $bucket -p $prefix
 
 sleep 3
 # READ
-ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=false --duration 30s --size $size -v $vol -b $bucket -p $prefix
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=false --duration 180s --size $size -v $vol -b $bucket -p $prefix
 
 sleep 3
 # 50% WRITE 50% READ
-ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=false --duration 30s --size $size -v $vol -b $bucket -p $prefix
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=false --duration 180s --size $size -v $vol -b $bucket -p $prefix
 
 # ONLY meta
 sleep 3
 # READ meta
-ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=true --duration 30s --size $size -v $vol -b $bucket -p $prefix
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 101 --read-metadata-only=true --duration 180s --size $size -v $vol -b $bucket -p $prefix
 
 sleep 3
 # 50% WRITE 50% READ meta
-ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=true --duration 30s --size $size -v $vol -b $bucket -p $prefix
+ozone freon ockrw -r 1000 -t $thread --linear --contiguous --percentage-read 50 --read-metadata-only=true --duration 180s --size $size -v $vol -b $bucket -p $prefix
 
